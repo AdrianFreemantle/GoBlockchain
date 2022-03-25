7bc5d7ab3a6441071b9c296bad6c03c7d6841ec3
Adrian Freemantle <adrian@synerics.com>
2022-03-25 10:48:47 +0200

    added balance calculations

--- blockchain.go
+++ blockchain.go
@@ -123,6 +123,21 @@ func (bc *Blockchain) Mining() bool {
 	return true
 }
 
+func (bc *Blockchain) CalcuateTotalAmount(blockchainAdress string) int64 {
+	var totalAmount int64 = 0
+	for _, b := range bc.chain {
+		for _, t := range b.transactions {
+			if blockchainAdress == t.recipientBlockchainAddress {
+				totalAmount += t.value
+			}
+			if blockchainAdress == t.senderBlockchainAddress {
+				totalAmount -= t.value
+			}
+		}
+	}
+	return totalAmount
+}
+
 func (bc *Blockchain) ProofOfWork() int {
 	transactions := bc.CopyTransactionPool()
 	previousHash := bc.LastBlock().Hash()
@@ -179,4 +194,12 @@ func main() {
 	bc.Mining()
 
 	bc.Print()
+
+	fmt.Printf("My balance: %d\n", bc.CalcuateTotalAmount(myBlockchainAddress))
+	fmt.Printf("A balance: %d\n", bc.CalcuateTotalAmount("A"))
+	fmt.Printf("B balance: %d\n", bc.CalcuateTotalAmount("B"))
+	fmt.Printf("C balance: %d\n", bc.CalcuateTotalAmount("C"))
+	fmt.Printf("D balance: %d\n", bc.CalcuateTotalAmount("D"))
+	fmt.Printf("E balance: %d\n", bc.CalcuateTotalAmount("E"))
+	fmt.Printf("F balance: %d\n", bc.CalcuateTotalAmount("F"))
 }
