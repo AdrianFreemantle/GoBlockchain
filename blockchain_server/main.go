872cddc0a36eaa8dac59fd94327c5694b9d117b8
Adrian Freemantle <adrian@synerics.com>
2022-03-27 11:53:59 +0200

    Added a basic UI for viewing wallets

--- blockchain_server/main.go
+++ blockchain_server/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"flag"
 	"log"
+	"fmt"
 )
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
-func main() {
+func main() {	
 	port := flag.Uint("port", 5000, "TCP Port Number for Blockchain Server")
 	flag.Parse()
 	app := NewBlockchainServer(uint16(*port))
+	fmt.Printf("Running Blockchain Server on port %d", app.Port())
 	app.Run()
 }
